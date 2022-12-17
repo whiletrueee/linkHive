f20297f5e81ab23e03033c44befb99d96ae7a322
flying-solo <flyingsoloharshit@gmail.com>
2022-12-17 22:10:21 +0530

    links

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,36 @@
+import { useState } from "react";
+import Icon from "../icons/icon_3.png";
+
 function Header(props) {
+  const [userName, setUsername] = useState("username");
+  const [mail, setMail] = useState("mail");
+
+  // eslint-disable-next-line no-undef
+  // eslint-disable-next-line no-undef
+  chrome.storage.local
+    .get(["data"])
+    .then((user) => {
+      setMail(user.data.user.email);
+      setUsername(user.data.user.email.split("@")[0]);
+    })
+    .catch((e) => console.log(e));
+
   return (
-    <div className="flex justify-between w-[400px] bg-black border-b-2 border-gray-700 p-2 text-white font-bold text-lg items-center">
-      Instant Share
+    <div className="flex justify-between w-[400px] bg-black border-b-2 border-gray-700 p-2 text-white font-bold text-base items-center">
+      Link Hive
       {props.signup ? (
-        <div
-          className="font-medium text-base hover:cursor-pointer hover:text-red-500"
-          onClick={() => props.setSignup(false)}
-        >
-          Logout
+        <div className="font-medium text-xs flex gap-1 items-center">
+          <div className="">{userName}</div>
+          <div className="">
+            <img
+              src={`https://avatars.dicebear.com/api/bottts/${mail}.svg`}
+              alt="display"
+              width={25}
+              height={25}
+            />
+          </div>
         </div>
-      ):null}
+      ) : null}
     </div>
   );
 }
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -2,7 +2,14 @@ import Share from "./Share";
 
 function Main() {
   return (
+    <>
+    <div className="flex text-white items-center justify-center">
+      <div className="flex border-r-2 border-gray-500">Send Link</div>
+      <div className="flex border-r-2 border-gray-500">Recieved Links</div>
+      <div className="flex">Sent Links</div>
+    </div>
       <Share />
+    </>
   );
 }
 
