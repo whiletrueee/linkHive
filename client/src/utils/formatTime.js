b1100f6a019a8ffdac4f918ae40494dce48bea87
shawshankkumar <shashankkumarthakur@gmail.com>
2022-12-19 17:11:51 +0530

    Fix: Export not working, fixed

--- client/src/utils/formatTime.js
+++ client/src/utils/formatTime.js
@@ -2,7 +2,6 @@ import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en";
 TimeAgo.addDefaultLocale(en);
 const timeAgo = new TimeAgo("en-US");
-
-const formatTime = (time) => {
+export const formatTime = (time) => {
   return timeAgo.format(+new Date(time));
 };
